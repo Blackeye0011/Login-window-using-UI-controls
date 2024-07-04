ae4e01dbde1d1b213a56bf2bccac84f32d5a8d3f
Blackeye0011 <comrade.78926@gmail.com>
2024-07-04 19:11:20 +0530

    Login window using UI controls

---
+++ app/src/main/java/com/example/a3rdpro/MainActivity.java
@@ -0,0 +1,38 @@
+package com.example.a3rdpro;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+public class MainActivity extends AppCompatActivity {
+    private EditText editTextUsername,editTextPassword;
+    private Button buttonLogin;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main);
+        editTextUsername = findViewById(R.id.editTextUsername);
+        editTextPassword = findViewById(R.id.editTextPassword);
+        buttonLogin = findViewById(R.id.buttonLogin);
+        buttonLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String username = editTextUsername.getText().toString().trim();
+                String password = editTextPassword.getText().toString().trim();
+                if(username.equals("Blackeye001") && password.equals("blog")){
+                    Toast.makeText(MainActivity.this, "Login successful",
+                            Toast.LENGTH_SHORT).show();
+                } else {
+                    Toast.makeText(MainActivity.this, "Invalid username or password",
+                            Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+    }
+}
